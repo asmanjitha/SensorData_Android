199136deb02541f59e1dac415ece9ad8811bee83
asmanjitha <asmanjitha@gmail.com>
2019-03-25 15:24:16 +0530

    Graph View

--- app/src/main/java/com/bitzmark/sensordata/MainActivity.java
+++ app/src/main/java/com/bitzmark/sensordata/MainActivity.java
@@ -14,9 +14,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
-    
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.List;
+
+import androidx.annotation.ColorInt;
+
+
+    public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
 
     private SensorManager sensorManager;
     private Sensor accelerometer;
@@ -32,6 +49,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView currentX, currentY,currentZ, accmeter;
     private Button button;
 
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,17 +61,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            // success! we have an accelerometer 0x
-            accmeter.setTextColor(Color.parseColor("#81c639"));
-            accmeter.setText("Accelerometer Registration Succefully!");
-
+            // success! we have an accelerometer
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         } else {
             // fai! we dont have an accelerometer!
-            accmeter.setTextColor(Color.parseColor("#ff1818"));
-            accmeter.setText("Accelerometer Registration Failed!");
 
         }
 
@@ -61,7 +77,62 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         currentY = (TextView) findViewById(R.id.currentY);
         currentZ = (TextView) findViewById(R.id.currentZ);
         button = (Button) findViewById(R.id.button);
-        accmeter = (TextView) findViewById(R.id.accmeter);
+        mChart = (LineChart) findViewById(R.id.chart1);
+
+        // enable description text
+        mChart.getDescription().setEnabled(true);
+        mChart.getDescription().setText("Accelerometer Data");
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.BLACK);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+        xl.setDrawAxisLine(false);
+
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.BLACK);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(20f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        feedMultiple();
 
     }
 
@@ -73,6 +144,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //onPause() unregister the accelerometer for stop listening the events
     protected void onPause() {
         super.onPause();
+        if (thread != null) {
+            thread.interrupt();
+        }
         sensorManager.unregisterListener(this);
     }
 
@@ -99,7 +173,149 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         deltaY = Math.abs(event.values[1]);
         deltaZ = Math.abs(event.values[2]);
 
+
+        if(plotData){
+            addEntry(event);
+            plotData = false;
+        }
+
     }
+
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+        LineData data2 = mChart.getData();
+        LineData data3 = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet2("X-Axis",-65281);
+                data.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0]), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+        if (data2 != null) {
+
+            ILineDataSet set = data2.getDataSetByIndex(1);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet2("Y-Axis", -16711936);
+                data2.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data2.addEntry(new Entry(set.getEntryCount(), event.values[1]), 1);
+            data2.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data2.getEntryCount());
+
+        }
+        if (data3 != null) {
+
+            ILineDataSet set = data3.getDataSetByIndex(2);
+
+            if (set == null) {
+                set = createSet2("Z-Axis", -16776961);
+                data3.addDataSet(set);
+            }
+
+            data3.addEntry(new Entry(set.getEntryCount(), event.values[2]), 2);
+            data3.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private LineDataSet createSet2(String label, int color) {
+
+        LineDataSet set = new LineDataSet(null, label);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(color);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null){
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
 
     public void displayCleanValues() {
         currentX.setText("0.0");
@@ -124,6 +340,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             state = true;
             button.setText("Stop");
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        sensorManager.unregisterListener(this);
+        thread.interrupt();
+        super.onDestroy();
     }
 
 }
